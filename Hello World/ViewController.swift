f9cf89ae4cb74c36b984f872c542ac006ca31f72
tomo072451 <apple07245135@gmail.com>
2022-05-03 14:19:49 +0900

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by 滝川 智弥 on R 4/05/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
